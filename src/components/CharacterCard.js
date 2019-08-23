65b3d42e41315420ce7c7ab93cae53fa5e3672aa
Angela Flowers <me@angela.flowers>
2019-08-23 11:46:39 -0400

    character API call working, building out character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
+import {Card, Image} from "semantic-ui-react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+  return (
+    <Card className="character-card">
+      <Image src={props.image} alt={props.name} />
+      <Card.Content>
+        <Card.Header>{props.name}</Card.Header>
+        <Card.Meta>
+          <span className="species-status">{props.species}; {props.status}.</span>
+          
+        </Card.Meta>
+      </Card.Content>
+    </Card>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState();
+
+  const [searchURL, setSearchURL] = useState("https://rickandmortyapi.com/api/character/")
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get(`${searchURL}`)
+    .then(response => {
+      console.log(response);
+      let charList = response.data.results;
+      console.log(charList);
+      setCharacters(charList);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+
+
+
+  }, [searchURL]);
 
   return (
     <section className="character-list grid-view">
