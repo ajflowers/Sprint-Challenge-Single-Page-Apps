eb03132497765ceb6ec9bc9839aa38caaabcecd8
Angela Flowers <me@angela.flowers>
2019-08-23 13:47:34 -0400

    MVP Completegit status!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-// import TabNav from "./components/TabNav.js";
+import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import CharacterList from "./components/CharacterList";
-import LocationsList from "./components/LocationsList";
-import EpisodesList from "./components/EpisodesList";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <EpisodesList />
-      {/* <LocationsList /> */}
-      {/* <CharacterList /> */}
-      {/* <TabNav /> */}
+      <TabNav />
     </main>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,11 +1,86 @@
-// import React from "react";
-// import { Tab, Menu, Icon } from "semantic-ui-react";
-// import { NavLink } from "react-router-dom";
+import React from "react";
+import { Tab, Menu, Icon } from "semantic-ui-react";
+import { NavLink, Route, Link } from "react-router-dom";
+import WelcomePage from "./WelcomePage";
+import CharacterList from "./CharacterList";
+import LocationsList from "./LocationsList";
+import EpisodesList from "./EpisodesList";
 
-// // TODO: Add missing tabs below
-// // Take a look at React Semantic UI tabs 
-// // https://react.semantic-ui.com/modules/tab/
-// export default function TabNav() {
+// TODO: Add missing tabs below
+// Take a look at React Semantic UI tabs 
+// https://react.semantic-ui.com/modules/tab/
 
-// };
+const panes = [
+    {
+        menuItem: {
+            as: NavLink,
+            content: "Home Page",
+            to: "/",
+            exact: true,
+            key: "home",
+            icon: "home"
+        },
+        render: () => (
+            <Route path="/" exact>
+                <Tab.Pane>
+                    <WelcomePage />
+                </Tab.Pane>
+            </Route>
+        )
+    },
+    {
+        menuItem: {
+            as: NavLink,
+            content: "Characters",
+            to: "/characters",
+            key: "characters",
+            icon: "user"
+        },
+        render: () => (
+            <Route path="/characters">
+                <Tab.Pane>
+                    <CharacterList />
+                </Tab.Pane>
+            </Route>
+        )
+    },
+    {
+        menuItem: {
+            as: NavLink,
+            content: "Locations",
+            to: "/locations",
+            key: "locations",
+            icon: "rocket"
+        },
+        render: () => (
+            <Route path="/locations">
+                <Tab.Pane>
+                    <LocationsList />
+                </Tab.Pane>
+            </Route>
+        )
+    },
+    {
+        menuItem: {
+            as: NavLink,
+            content: "Episodes",
+            to: "/episodes",
+            key: "episodes",
+            icon: "tv"
+        },
+        render: () => (
+            <Route path="/episodes">
+                <Tab.Pane>
+                    <EpisodesList />
+                </Tab.Pane>
+            </Route>
+        )
+    }
+]
+
+export default function TabNav() {
+    return (
+    <Tab panes={panes} />
+    );
+};
 
